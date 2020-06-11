13863e4d3fcfaacff6b2b60ab7c5c2c1579193f7
John Woo <jdwoo.88@yahoo.com>
2020-06-12 06:37:17 +0800

    NavigationView

--- app/src/main/java/com/jwoo/materialdesign/MainActivity.kt
+++ app/src/main/java/com/jwoo/materialdesign/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.jwoo.materialdesign
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private var toolbar: Toolbar? = null
+    private var drawerlayout: DrawerLayout? = null
+    private var navigationView: NavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = main_toolbar
+        setSupportActionBar(toolbar)
+
+        drawerlayout = drawer_layout
+        navigationView = nav_view
+
+        var actionBarDrawerToggle: ActionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerlayout,
+            toolbar,
+            R.string.openNavDrawer,
+            R.string.closeNavDrawer
+        )
+
+        drawerlayout!!.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        navigationView!!.setNavigationItemSelectedListener(this)
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return false
+    }
+
 }
